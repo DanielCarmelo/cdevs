60a5f7c70bfb9d1c8a73f5dc1965f776a055b1ba
DanielCarmelo <132840951+DanielCarmelo@users.noreply.github.com>
2025-07-03 19:03:56 -0300

    Add files via upload

---
+++ Bloque 1/1_1 Introducción/Desadio 1 suma de dos numeros.py
@@ -0,0 +1,17 @@
+# Define dos variables con números específicos asignados por ti.
+# Suma esos dos números y almacena el resultado en una tercera variable.
+# Muestra el resultado de la suma.
+
+
+a = 11    #defino variable a
+b = 10    #defino variable b
+suma = a + b  # realizo la operación correspondiente y se la asigno a la variable suma
+print(suma)   # imprimo en la pantalla el valor de la suma
+
+#  Otro experimento, con numeros elegidos por el usuario
+
+a = float(input("Por favor, ingresa el valor de a: "))
+b = float(input("Por favor, ingresa el valor de b: "))
+input("Presiona Enter para realizar el calculo...")
+suma = a + b
+print("El total es: ", int(suma))
